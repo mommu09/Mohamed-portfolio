baafa667a8808005a1712a2a71599b9d190effd2
mommu09 <mohamedj0910@gmail.com>
2024-09-14 23:39:45 +0530

    Hamburger menu added

---
+++ index.js
@@ -0,0 +1,33 @@
+const menu = document.querySelector('.menu-btn');
+const menuList = document.querySelector('nav');
+const icon = document.querySelector('.icon');
+const links = document.querySelectorAll('nav ul li a');
+menu.addEventListener('click', () => {
+  if (menuList.style.left === '0px') {
+    menuList.style.left = '-100dvw';
+    icon.classList.add('fa-bars');
+    icon.classList.remove('fa-xmark');
+    icon.style.color="white";
+  } else {
+    menuList.style.left = '0px';
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-xmark');
+    icon.style.color="crimson";
+  }
+});
+
+// links.addEventListener('click', () => {
+//   menuList.style.left='-100dvw';
+//   icon.classList.add('fa-bars');
+//   icon.classList.remove('fa-xmark');
+//   icon.style.color="white";
+// });
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    menuList.style.left = '-100dvw';
+    icon.classList.add('fa-bars');
+    icon.classList.remove('fa-xmark');
+    icon.style.color = "white";
+  });
+});
